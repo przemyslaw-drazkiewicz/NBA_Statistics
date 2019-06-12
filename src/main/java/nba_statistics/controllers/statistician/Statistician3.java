cfbc85eb916f829ee4501e4e2013e7bc11996861
AKincel18 <akincel97@gmail.com>
2019-06-12 19:11:47 +0200

    added match results during collect statistics, added quarters and extra time to statistician view

--- src/main/java/nba_statistics/controllers/statistician/Statistician3.java
+++ src/main/java/nba_statistics/controllers/statistician/Statistician3.java
@@ -10,27 +10,21 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import nba_statistics.controllers.AccountController;
 import nba_statistics.entities.Matches;
 import nba_statistics.entities.PlayerMatchAchievements;
 import nba_statistics.entities.Players;
-import nba_statistics.entities.Teams;
 import nba_statistics.services.MatchesService;
 import nba_statistics.services.PlayerMatchAchievementsService;
-import nba_statistics.services.PlayersService;
 
 import java.io.IOException;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static nba_statistics.others.Alerts.*;
 
 public class Statistician3 implements Initializable {
 
@@ -62,6 +56,17 @@ public class Statistician3 implements Initializable {
     @FXML private Text min;
     @FXML private Text colon;
 
+    //Results
+    @FXML private Text pointsHome;
+    @FXML private Text pointsAway;
+
+    //quater
+    @FXML private Text couter;
+    @FXML private Text txt;
+
+    //info
+    @FXML private Text info;
+
     private List<Players> playersTeamH;
     private List<Players> playersTeamA;
 
@@ -70,6 +75,7 @@ public class Statistician3 implements Initializable {
     private String matchT2;
 
     private int extraTimeCount = 0;
+    private int quarterCount = 1;
 
     private ObservableList<String> playersSquadA = FXCollections.observableArrayList();
     private ObservableList<String> playersSquadH = FXCollections.observableArrayList();
@@ -128,18 +134,26 @@ public class Statistician3 implements Initializable {
     }
     public void oneH(){
         String player =(String) homeTeam.getSelectionModel().getSelectedItem();
-        if(player!=null) //player is not selected
+        if(player!=null) { //player is not selected
             home.get(findPosPlayerHome(player)).addPoints(1);
+            pointsHome.setText(String.valueOf((Integer.parseInt(pointsHome.getText()) + 1)));
+        }
+
     }
     public void twoH(){
         String player =(String) homeTeam.getSelectionModel().getSelectedItem();
-        if (player != null)
+        if (player != null){
             home.get(findPosPlayerHome(player)).addPoints(2);
+            pointsHome.setText(String.valueOf((Integer.parseInt(pointsHome.getText()) + 2)));
+        }
+
     }
     public void threeH(){
         String player =(String) homeTeam.getSelectionModel().getSelectedItem();
-        if (player!=null)
+        if (player!=null) {
             home.get(findPosPlayerHome(player)).addPoints(3);
+            pointsHome.setText(String.valueOf((Integer.parseInt(pointsHome.getText()) + 3)));
+        }
     }
     public void stealsH(){
         String player =(String) homeTeam.getSelectionModel().getSelectedItem();
@@ -179,18 +193,24 @@ public class Statistician3 implements Initializable {
     }
     public void oneA(){
         String player =(String) awayTeam.getSelectionModel().getSelectedItem();
-        if(player!=null) //player is not selected
+        if(player!=null) { //player is not selected
             away.get(findPosPlayerAway(player)).addPoints(1);
+            pointsAway.setText(String.valueOf((Integer.parseInt(pointsAway.getText()) + 1)));
+        }
     }
     public void twoA(){
         String player =(String) awayTeam.getSelectionModel().getSelectedItem();
-        if (player != null)
+        if (player != null) {
             away.get(findPosPlayerAway(player)).addPoints(2);
+            pointsAway.setText(String.valueOf((Integer.parseInt(pointsAway.getText()) + 2)));
+        }
     }
     public void threeA(){
         String player =(String) awayTeam.getSelectionModel().getSelectedItem();
-        if (player!=null)
+        if (player!=null) {
             away.get(findPosPlayerAway(player)).addPoints(3);
+            pointsAway.setText(String.valueOf((Integer.parseInt(pointsAway.getText()) + 3)));
+        }
     }
     public void stealsA(){
         String player =(String) awayTeam.getSelectionModel().getSelectedItem();
@@ -230,6 +250,7 @@ public class Statistician3 implements Initializable {
 
         MatchesService matchesService = new MatchesService();
         matchesService.update(match);
+        myTimer.cancel();
 
 
 
@@ -265,13 +286,17 @@ public class Statistician3 implements Initializable {
 
     //STOPPER
     private int seconds =0;
-    private int minutes= 0;
-    private int []matchTime= {0,10}; //zero pos -> minutes, first pos -> seconds, now match duration= 10sec
+    private int minutes= 0;;
+    private int [] matchPartTime = {0,10}; //zero pos -> minutes, first pos -> seconds, now quarter duration= 10sec
+    private int stopMinutes = matchPartTime[0];
+    private int stopSeconds = matchPartTime[1];
+    private int [] extraTime = {0,0};
     private Timer myTimer = new Timer();
     private TimerTask task = new TimerTask() {
         @Override
         public void run() {
             if (isStart) {
+                info.setVisible(false);
                 seconds = Integer.parseInt(sec.getText());
                 if (seconds != 59){
                     seconds++;
@@ -289,17 +314,47 @@ public class Statistician3 implements Initializable {
                 else
                     min.setText(0 + String.valueOf(minutes));
 
-                if (matchTime[0] == minutes && matchTime[1] == seconds) { //MATCH FINISHED (or not)
+
+
+
+                if ( stopMinutes == minutes && stopSeconds == seconds) { //Quarter FINISHED
+
                     isStart = false;
+                    if (quarterCount == 4) { //end match (or no)
+                        if (getPoints(home) == getPoints(away)) //extra time, match not finished
+                        {
+                            extraTimeCount++;
+                            extraTime[1]=5; // for example: extra time duration = 5 sec xd
+                            txt.setText("extra time");
+                            couter.setText(String.valueOf(extraTimeCount));
+                            info.setVisible(true);
+                            info.setText("DRAW! Press 's' to start " + extraTimeCount + " extra time");
 
-                    if (getPoints(home) == getPoints(away)) //extra time, match not finished
-                    {
-                        extraTimeCount++;
-                        matchTime[1]+=5; // for example: extra time duration = 5 sec xd
-
+                        }
+                        else {
+                            info.setVisible(true);
+                            info.setText("Match finished!");
+                            finishedMatch();
+                        }
                     }
-                    else
-                        finishedMatch();
+                    else{
+
+                        info.setVisible(true);
+                        info.setText(quarterCount + " quarter finished! Press 's' to start next quarter");
+                        quarterCount++;
+                        txt.setText("quarter");
+                        couter.setText(String.valueOf(quarterCount));
+                    }
+
+                    stopSeconds = matchPartTime[1]*quarterCount + extraTime[1] * extraTimeCount;
+                    if (stopSeconds >=60){
+                        stopMinutes = stopSeconds / 60;
+                        stopSeconds = stopSeconds % 60;
+                    }else
+                        stopMinutes = matchPartTime[0]*quarterCount + extraTime[0] * extraTimeCount;
+
+
+
 
 
 /*                isStart = false;
